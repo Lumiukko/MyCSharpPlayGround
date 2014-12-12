2380c72fdbffe7cd95199e9cfc88075120dba958
Lumiukko <makko@brainscorch.net>
2014-12-12 08:18:51 +0100

    added Store class

---
+++ MyCSTest/Store.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSTest
+{
+    public class Store
+    {
+
+        public string Name { get; private set; }
+        public string Chain { get; private set; }
+        public string Address { get; private set; }
+
+        public Store(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name + " (" + Chain + ")" + (!Address.Equals("") ? ", " + Address : "");
+        }
+
+    }
+}
